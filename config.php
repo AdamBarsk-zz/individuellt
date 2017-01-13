2afbe45b8d6ee858911a85aa6775db237af0e48f
Adam Barsk <adambarsk@gmail.com>
2017-01-13 10:39:32 +0100

    Delete config.php

--- config.php
+++
@@ -1,7 +0,0 @@
-<?php
-  define('DB_SERVER', 'barsk-219289.mysql.binero.se');
-  define('DB_USERNAME', '219289_yf85187');
-  define('DB_PASSWORD', 'hejhej123');
-  define('DB_DATABASE', '219289-barsk');
-  $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
-?>
