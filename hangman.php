c7a782f4effab064de747ba5ed35964920de900f
AdamBarsk <adambarsk@gmail.com>
2017-01-25 20:20:02 +0100

    hangman: done

--- hangman.php
+++ hangman.php
@@ -3,9 +3,10 @@ echo '
 <div id="hangman" class="modal">
   <div class="modal-hangman">
     <div class="hangman-title">
-      <h1 id="hangman-status"></h1>
+      <h1 class="hangman-status" id="hangman-status"></h1>
     </div>
     <div>
+      <div class="hangman-picture" id="hangman-picture"></div>
       <h1 class="hangman-lives" id="hangman-lives"><h1>
     </div>
     <div>
@@ -13,6 +14,9 @@ echo '
     </div>
     <div>
       <h2 class="hangman-guess" id="hangman-guess"></h2>
+    </div>
+    <div class="container-inputs">
+      <button type=submit id="hangman-button" class="login-button play-again"><b>Play again</b></button>
     </div>
   </div>
 </div>';
--- head.php
+++ head.php
@@ -8,7 +8,7 @@ echo '
   <meta name="description" content="Swedish Front End Developer"/>
   <link rel="stylesheet" href="styles/style.css"/>
   <link rel="shortcut icon" type="image/x-icon" href=""/content/favicon.ico" />
-  <link link="link" href="https://fonts.googleapis.com/css?family=Fira+Sans:400,400i,700,700i" rel="stylesheet"/>
+  <link href="https://fonts.googleapis.com/css?family=Fira+Sans:400,400i,700,700i" rel="stylesheet"/>
   <link rel="stylesheet" href="https://use.fontawesome.com/45faac0910.css"/>
   <script src="https://code.jquery.com/jquery-3.1.1.min.js" integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous"></script>
   <script src="/scripts/hangman.js" type="text/javascript"></script>
